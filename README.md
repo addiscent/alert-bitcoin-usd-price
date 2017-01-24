c06519393c361975db94807b8d46d434a75c336c
Rex Addiscentis <raddiscentis@addiscent.com>
2017-01-23 22:17:27 -0800

    Add feature which also compares BTC price to greater than value

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # alert-bitcoin-usd-price
-This Bash script sends notification if the price of bitcoin falls below a user specified set price.  When run periodically as a _cron_ job, it may serve as a "buy" alert.
+This program compares the price of Bitstamp "btcusd" to a condition-price value specified by the user.  An example condition-price is '<789.00' or '>1100.00'.  If the Bitstamp "btcusd" price satisfies the alert condition, (less than or greater than the specified value), several notification actions may be performed.  When run periodically as a _cron_ job, this program may serve as a "buy" or a "sell" alert.
 
-This program fetches the current _BID_ price of Bitstamp "btcusd" from a ticker and compares it against an alarm _SET_ value.  If the _BID_ Bitstamp "btcusd" price is _lower_ than the alarm _SET_ value, notification is performed as follows:
+The program fetches the current _BID_ price of Bitstamp "btcusd" from a ticker and applies a condition-price value to the BTC price.  If the Bitstamp "btcusd" satisfies the condition-price specified, user notification is performed as follows:
 
-  1. Bitcoin _BID_ price and alert _SET_ value are output to _STDOUT_
+  1. Bitcoin BTC price and condition-price alert value are output to _STDOUT_
   2. An alert notification is sent to the _GUI_ (Desktop)
   3. Email notification is sent if enabled
   4. If enabled, a notification message is sent to an AWS SNS Topic via the AWS CLI
@@ -16,14 +16,22 @@ The source of Bitcoin price data is _https://www.bitstamp.net_.
 After cloning this project or downloading/unzipping the project zip file, copy _alert-btcusd-price.sh_ into a directory in which the user has permissions, and ensure the file permission is set as _executable_.
 
 ### Basic Command Line Syntax
-The alarm price _SET_ point in this example below is specified as _789_.  Change this value to set the price at which notification is performed.  If email notification is desired, set the user's email address in place of the email address in the example below.  If no email notification is desired, remove the email address "example@example.com".  If "example@example.com" is not removed from the example below, the host will waste CPU and IO cycles pointlessly attempting to send unnecessary email, and email notifications of delivery failures may arrive in the host admin's mailbox.
+The condition-price in this example below is specified as _'<789.00'_.  Thise means send a notification when the BTC price falls below $789.99.  Change this value to set the price at which notification is performed.  If email notification is desired, set the user's email address in place of the email address in the example below.  If no email notification is desired, remove the email address "example@example.com".  If "example@example.com" is not removed from the example below, the host will waste CPU and IO cycles pointlessly attempting to send unnecessary email, and email notifications of delivery failures may arrive in the host admin's mailbox.
 
-        alert-btcusd-price.sh 789 example@example.com
+        alert-btcusd-price.sh '<789.99' example@example.com
 
-### Used As A Buy Alert
+Alternately, the program may be used to notify the user if the BTC price rises above a specified value.  The condition-price in this example below is specified as _'>1234.99'_.  Thise means send a notification when the BTC price rises above $1200.  Change this value to set the price at which notification is performed.
+
+        alert-btcusd-price.sh '>1234.99' example@example.com
+
+Optionally, numerals after the decimal may be omitted, e.g., values of '<789' or '>1234' are accepted.
+
+Note that the single quotes surrounding the condtion-price argument are required.  If the single quotes are omitted the program may behave in and undesireable manner.
+        
+### Used As A Buy Or Sell Alert
 If the user wishes to have the price checked regularly by cron, add the line of code shown below to the user's crontab .  The example line below causes the script to be executed once per 10 minutes.  Modify the example below accordingly if the script should be run more or less often.  The example code shown below assumes the user has alert-btcusd-price.sh stored in a directory named "bin", in the "user" home directory. If stored in some other path, adjust this example accordingly.  
 
-        */10 * * * * /home/user/bin/alert-btcusd-price.sh 789 example@example.com >/dev/null 2>&1
+        */10 * * * * /home/user/bin/alert-btcusd-price.sh '<789.99' example@example.com >/dev/null 2>&1
 
 If the user wishes to have a notification message sent to an AWS SNS Topic, the following environment variable must be defined *in the crontab file*, as given in this example :
 
--- alert-btcusd-price.sh
+++ alert-btcusd-price.sh
@@ -1,82 +1,137 @@
 #!/bin/bash
-# This program compares the price of Bitstamp "btcusd" against an alarm value.
-# If the Bitstamp "btcusd" price is lower than the alarm value, the bid price
-# and set price are displayed to STDOUT, an alert notification is sent to
-# the GUI (Desktop), email notification is sent, and if an environment variable
-# is defined, a notification message is sent to that AWS SNS Topic.
+# This program compares the price of Bitstamp "btcusd" against an alert
+# condition-price.  An example condition-price is '<789.00' or '>1100.00'.
+# If the Bitstamp "btcusd" price satisfies the alert condition, the BTC price
+# and trigger condition-price are displayed to STDOUT, an alert notification is
+# sent to the GUI (Desktop), email notification is sent, and if an environment
+# variable is defined, a notification message is sent to that AWS SNS Topic.
 #
 # The source of the price data is https://www.bitstamp.net
 #
 # This script has been tested only on Ubuntu 14.10 DESKTOP version.
 #
-# rex addiscentis  2017/01/19
+# rex addiscentis 2017/01/23
 # Apache 2.0 License
 # https://github.com/addiscent
 
-# in case of multiple hosts sending notifications, set its name here
+# in case of multiple hosts sending notifications,
+# set sending host name here
 btcusd_price_hostname="Esmerelda"
 
+############  constants  ############
 
-# must specify an alarm "set" price on command line
-if [ $# -lt 1 ]; then
-  echo "Usage   :  alert-btcusd-price.sh  price  [emailaddress]"
-  echo "Example :  alert-btcusd-price.sh  789    example@example.com"
-  echo "AWS SNS Topic environment variable example : BTCUSD_PRICE_ALERT_AWS_SNS_TOPIC="
-  echo "    arn:aws:sns:us-west-2:NNNNNNNNNNNN:bitcoin-usd-price-alert"
-  echo "Alarm \"set\" value required, exiting, nothing done"
-  exit 1
-fi
+TRUE="1"
+OKAY="0"
 
-#set -x
+ERR_USAGE="1"
+ERR_SUSPECT_PRICE="2"
 
-# fetch the json block containing current bitstamp btcusd prices and
-# extract "bid" price
-bid=$(curl https://www.bitstamp.net/api/v2/ticker/btcusd/ | awk -F ': "' '{print $5}' | cut -c1-6)
+NUM_ARGS="$#"
+COMPARE_PRICE="$1"
+NOTIFY_EMAIL_ADDRESS="$2"
 
-# for debug trace or whatever
-echo "BID price is    : \$$bid"
-echo "Alarm SET price is : \$$1"
+############  function return value variables  ############
+validate_usage=""
+notify_suspect_btc_price=""
+
+
+######################## Begin Function Definitions  #########################
+
+# script invocation must specify a condition-price on command line.
+# return val of func is in script variable of same name as func
+function validate_usage () {
+  if [ $NUM_ARGS \< 1 ]; then
+    echo "Usage   :  alert-btcusd-price.sh  condition-price  [emailaddress]"
+    echo "Example 1 :  alert-btcusd-price.sh  '<789.00'  example@example.com"
+    echo "Example 2 :  alert-btcusd-price.sh  '>1100.00'  example@example.com"
+    echo "AWS SNS Topic environment variable example : BTCUSD_PRICE_ALERT_AWS_SNS_TOPIC="
+    echo "    arn:aws:sns:us-west-2:NNNNNNNNNNNN:bitcoin-usd-price-alert"
+    echo "Alarm \"set\" condition-price required.  Exiting, nothing done"
+    validate_usage="$ERR_USAGE"
+    return
+  fi
+  validate_usage="$OKAY"
+}
+
 
 # https://www.bitstamp.net service could have been unavailable during fetch.
-# Detect and exit if suspect
-if [ "$bid" \< "1" ] ; then
-  if [ $# = 2 ] ; then
-  mail -s "Bitcoin Price Alert ($btcusd_price_hostname)" "$2" << EOF
+# Detect if suspect.
+# return val of func is in script variable of same name as func
+function notify_suspect_btc_price () {
+#  btc_price=0 # for testing only
+  if [ "$btc_price" \< "1" ] ; then
+    if [ "$NOTIFY_EMAIL_ADDRESS." != "." ] ; then
+  mail -s "Bitcoin Price Alert ($btcusd_price_hostname)" "$NOTIFY_EMAIL_ADDRESS" << EOF
 From Bitcoin Price Monitor - Notification :
 
    Suspect data received from :
      https://www.bitstamp.net/api/v2/ticker/btcusd/
    Service may be unavailable.
-   Bitcoin BID price : \$$bid
+   Bitcoin BTC price : \$$btc_price
 EOF
+    fi
+    notify_suspect_btc_price="$ERR_SUSPECT_PRICE"
+    return
   fi
-  echo "Suspect data received, service may be unavailable, exiting"
-  exit 0
-fi
+  notify_suspect_btc_price="$OKAY"
+}
 
-# test for price alarm trigger and do notification if necessary
+
+# test for condition-price alert trigger and do notification if necessary.
 # use calculator (bc) for floating point compare test.
-true="1"
-
-if [ $(echo "$bid < $1" | bc) == $true ] ; then
-  # notify on GUI desktop
-  DISPLAY=:0 notify-send "Bitcoin Price Notification" "Bitcoin BID price \$$bid\nis below SET Alarm \$$1"
-  # if an email address is provided on cmd line, send notification via email
-  if [ $# = 2 ] ; then
-    mail -s "'Bitcoin Price Alert ($btcusd_price_hostname)" "$2" << EOF
+function notify_btc_price () {
+  if [ $(echo "$btc_price$COMPARE_PRICE" | bc) == $TRUE ] ; then
+    # notify on GUI desktop
+    DISPLAY=:0 notify-send "Bitcoin Price Notification" "Bitcoin BTC price \$$btc_price\ntriggers condition-price alert '$COMPARE_PRICE'"
+    # if an email address is provided on cmd line, send notification via email
+    if [ "$NOTIFY_EMAIL_ADDRESS." != "." ] ; then
+    mail -s "'Bitcoin Price Alert ($btcusd_price_hostname)" "$NOTIFY_EMAIL_ADDRESS" << EOF
 From Bitcoin Price Monitor - Notification :
 
-   Bitcoin BID price \$$bid,
-   is below alert SET price \$$1
+   Bitcoin BTC price \$$btc_price,
+   triggers condition-price alert "$COMPARE_PRICE"
 EOF
+    fi
+    # if an AWS SNS topic is defined in a parent ENV variable,
+    # send a notification to it
+    if [ "$BTCUSD_PRICE_ALERT_AWS_SNS_TOPIC." != "." ] ; then
+      aws sns publish --topic-arn "$BTCUSD_PRICE_ALERT_AWS_SNS_TOPIC" --message "Bitcoin BTC price \$$btc_price triggers condition-price alert '$COMPARE_PRICE'" --output json
+    fi
   fi
-  # if an AWS SNS topic is defined in an ENV variable, send a notification to it
-  if [ "$BTCUSD_PRICE_ALERT_AWS_SNS_TOPIC." != "." ] ; then
-    aws sns publish --topic-arn "$BTCUSD_PRICE_ALERT_AWS_SNS_TOPIC" --message "Bitcoin BID price \$$bid is below SET Alarm \$$1" --output json
-  fi
-fi
+}
+
+######################### End Function Definitions  ##########################
+
+
+################################ Begin MAIN  #################################
+
+#set -x
+
+# must specify an alert "set" condition-price on command line.
+# return val of func is in script variable of same name as func
+validate_usage 
+if [ "$validate_usage" == "$ERR_USAGE"  ]; then exit $ERR_USAGE ; fi
+
+# fetch the json block containing current bitstamp btcusd prices and
+# extract "bid" price
+btc_price=$(curl https://www.bitstamp.net/api/v2/ticker/btcusd/ | awk -F ': "' '{print $5}' | cut -c1-6)
+
+# for debug trace or whatever
+echo "BTC price : \$$btc_price"
+echo "Alert condition-price trigger : $1"
+
+# https://www.bitstamp.net service could have been unavailable during fetch.
+# Detect and exit if suspect.
+# return val of func is in script variable of same name as func
+notify_suspect_btc_price
+if [ "$notify_suspect_btc_price" == "$ERR_SUSPECT_PRICE"  ]; then exit $ERR_SUSPECT_PRICE ; fi
+
+# test for condition-price alert trigger and do notification if necessary
+notify_btc_price
 
 #set +x
 
 exit 0
+
+################################# End MAIN  ##################################
 
